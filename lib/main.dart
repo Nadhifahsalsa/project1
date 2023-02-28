9219064a388e3436d2bd97a99be8e27fb15c7266
Nadhifahsalsa <salsabilanadhifah83@gmail.com>
2023-02-28 09:14:14 +0700

    28 februari

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+      MaterialApp(
+          title: "Aplikasi Flutter",
+          home: Scaffold(
+            appBar: AppBar(title: Text('Aplikasi Flutter'),),
+            body: Material(
+              color: Colors.white,
+              child: Center(
+                child: Text(
+                  "Nadhifah",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(color: Colors.black, fontSize: 40.0),
+                ),
+              ),
+            ),
+          )
+      ));
+}
